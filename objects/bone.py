5b2854463a4423a6a6b010390d85ebc3afea6b27
REDxEYE <med45c@gmail.com>
2022-02-22 22:42:45 +0300

    Update for bone axis

--- objects/bone.py
+++ objects/bone.py
@@ -11,7 +11,7 @@ class Bone:
 
     @property
     def blender_quat(self):
-        return self.quat[3], self.quat[0], self.quat[1], self.quat[2]
+        return self.quat[3], self.quat[2], self.quat[0], self.quat[1]
 
     def __repr__(self):
         return 'Bone(name=%r, parent_id=%r, pos=%r, quat=%r)' % (self.name, self.parent_id, self.pos, self.quat)
